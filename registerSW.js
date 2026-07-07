baebd31f48c4d812b36f62bf23a53b962f9e0829
READY Engineer <todent5@gmail.com>
2026-07-07 17:53:48 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/pareto_language/sw.js', { scope: '/pareto_language/' })})}
